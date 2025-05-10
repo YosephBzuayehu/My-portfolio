dac09b4634d99be687fdd08e4f25ee627d46256c
Yoseph <yoseph2719@gmail.com>
2025-05-10 15:53:43 +0300

    my portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+// ============ typing Animation ===========
+var typed = new Typed(".typing", {
+  strings: [" ", "Student", "Web Developer", "Graphics Designer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
